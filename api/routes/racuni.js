cf411774971840d90b6d581535cdc40ae2342c57
demir6693 <demir_agovic@live.com>
2018-12-11 12:34:05 +0100

    deploy

---
+++ api/routes/racuni.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: '5.79.70.193',
+    user: 'root',
+    password: 'IDMrcxUOSoup',
+    database: 'mobile_town'
+});
+
+db.connect((err => {
+    if(err)
+    {
+        throw err;
+    }
+    console.log('MySql Connected...');
+}));
+
+/*
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET req racuni'
+    });
+}); */
+
+router.get('/', (req, res) => {
+    let sql = "SELECT * FROM racuni";
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        
+        res.send(result);
+    });
+});
+
+//dnevni
+router.get('/getdnevni', (req, res) => {
+    var datetime = new Date();
+    var current_day = datetime.getDate();
+    var current_month = datetime.getMonth() + 1;
+    var current_year = datetime.getFullYear();
+    let sql = `SELECT * FROM racuni WHERE ( (DAY(datum_izdavanja)) = ${current_day}
+    AND (MONTH(datum_izdavanja)) = ${current_month}) 
+    AND (YEAR(datum_izdavanja) = ${current_year})`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+//mesecni
+router.get('/getmesecni', (req, res) => {
+    var datetime = new Date();
+    var current_month = datetime.getMonth() + 1;
+    var current_year = datetime.getFullYear();
+    let sql = `SELECT (SUM(iznos)- SUM(iznos_nabavna)), SUM(iznos) FROM racuni WHERE ((MONTH(datum_izdavanja)) = ${current_month}) 
+    AND (YEAR(datum_izdavanja) = ${current_year})`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+//godisnji
+router.get('/getgodisnji', (req, res) => {
+    var datetime = new Date();
+    var current_year = datetime.getFullYear();
+    let sql = `SELECT (SUM(iznos)- SUM(iznos_nabavna)), SUM(iznos) FROM racuni WHERE (YEAR(datum_izdavanja) = ${current_year})`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const racuniRoutes = require('./api/routes/racuni');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+});
+
+app.use('/racuni', racuniRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+module.exports = app;
